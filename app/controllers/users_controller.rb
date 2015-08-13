4a6d787aac5290406075fd1256c448ee06108933
Vimlesh Sharma <Vimlesh.Sharma@gmail.com>
2015-08-13 16:18:05 +0530

    Adding AJAX on create/delete and error message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,14 +25,20 @@ class UsersController < ApplicationController
   # POST /users
   # POST /users.json
   def create
-    @user = User.new(user_params)
-
+    logger.info "Processing the request..."
+    @user = User.new(user_params)    
     respond_to do |format|
       if @user.save
         format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.js {}
         format.json { render :show, status: :created, location: @user }
       else
+        # @user.errors.messages.each do |key, value|
+          # logger.debug(key)
+          # logger.debug(value)
+        # end
+        
+        format.js {}        
         format.html { render :new }
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
